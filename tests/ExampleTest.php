f20b7d0a2b45173fc0cc234d9a79c3aefc29ffe1
oomori-you <valsamina@gmail.com>
2016-05-24 19:08:37 +0900

    add test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -16,4 +16,10 @@ class ExampleTest extends TestCase
         $this->visit('/')
              ->see('Laravel 5');
     }
+	
+	public function testHello()
+    {
+        $this->visit('/hello')
+             ->see('hello world');
+    }
 }
